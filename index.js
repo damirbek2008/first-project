9dc4ac2b88e6ea61898dc319b3b3078e1ffe0b76
damirbek2008 <damirbekkamoliddinov2008@gmail.com>
2024-08-08 09:50:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+// let name ='';
+
+// while(name==='' || name===null){
+//     name=window.prompt("Enter your name")
+// }
+
+// console.log(name)
+
+
+// let movie='';
+// while(movie==='' || movie===null){
+//     movie=window.prompt("Enter how many movie have you seen")
+//     movie=Number(movie)
+
+//     if  (movie<=5)    {
+//         window.alert("You are imposter")
+//     }else if (movie>5 && movie<=10) {
+//         window.alert("You are becoming legend")
+//     }else if(movie>10 && movie<=15) {
+//         window.alert("You are on the berch of danger")
+//     }else{
+//         window.alert("You are wasting your time")
+
+// }
+// }
+// console.log(name)
+// console.log(movie)
+
+
+// document.getElementById('myH1').textContent=`Hello my friend ${name}`
+// document.getElementById('myH2').textContent=`You have seen atleast ${movie} movies`
+
+// if  (movie<=5)    {
+//     document.getElementById('myH3').textContent= ("You are imposter")
+// }else if (movie>5 && movie<=10) {
+//     document.getElementById('myH3').textContent=("You are becoming legend")
+// }else if(movie>10 && movie<=15) {
+//     document.getElementById('myH3').textContent=("You are on the berch of danger")
+// }else if(movie>15){
+//     document.getElementById('myH3').textContent=("You are wasting your time")
+// }
+
+
+/*
+let phoneNumber;
+let name1;
+
+document.getElementById("button").onclick = function(){
+    phoneNumber = document.getElementById("input").value;
+    name1= document.getElementById("name1").value;
+    document.getElementById("myH3").textContent=`Hello ${name1} Now I get your phone number ${phoneNumber}. Now I can steal all money you have from your bank account`
+}
+*/
+
+/*
+let PI=3.14159;
+let radious;
+let size;
+
+document.getElementById("buttonCircle").onclick=function(){
+    radious=document.getElementById("input1").value;
+    radious=Number(radious)
+    size=2*PI*radious
+
+    document.getElementById("H3").textContent= size + 'sm'
+}
+*/
+
